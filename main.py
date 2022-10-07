66fb177a78d7783d5b5aca695f9d8ef2fabc9326
jeanatte-khs <102945064+jeanatte-khs@users.noreply.github.com>
2022-10-07 23:16:53 +0600

    second commit

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import os
 import pandas as pd
-from openpyxl import load_workbook
 import os, glob
+
 
 path = r'C:\\Users\\jeana\\PycharmProjects\\testtask'
 directory = []
 filename = []
+extensions = []
 
 for (root, dirs, file) in os.walk(path):
     for f in file:
         directory.append(root)
         filename.append(f)
+        ext = os.path.splitext(f)
+        extensions.append(ext[1][1:])
         print(f)
 
-df = pd.DataFrame(list(zip(directory, filename)), columns=['Directory', 'filename'])
+
+df = pd.DataFrame(list(zip(directory, filename, extensions)), columns=['Directory', 'filename', 'extensions'])
 df.to_excel('result.xlsx')
